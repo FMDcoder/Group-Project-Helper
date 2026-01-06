962d38b5021e6c080913d445f07fee832d33db9d
joeron <joeron@kth.se>
2026-01-06 15:30:37 +0100

    made projects clickable in project selector

--- src/components/CurrentProjects.vue
+++ src/components/CurrentProjects.vue
@@ -3,19 +3,27 @@
     <h2>Active projects:</h2>
     <ul>
      <li v-for="p in getProjects()" :key="p.id">
-       {{ p.name }} {{ p.progress }}
+       <router-link to="/project" @click="">
+        {{ p.name }}
+      </router-link>
+      {{ p.progress }}
      </li>
     </ul> 
   </div>
 </template>
 
 <script>
+import ProjectView from '@/views/ProjectView.vue';
+
   export default {
     props: ['model'],
     methods: {
       getProjects() {
         return this.model.getProjects();
       },
+      setCurrentProject(id) {
+        this.model.setCurrentProject(id);
+      }
     },
   }
 </script>
