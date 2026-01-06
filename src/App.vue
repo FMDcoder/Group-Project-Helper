14e0f21d5790a3f6a433cb221e7f5e1917e3a5f2
Vanessa Nilsson <nilssonik18@gmail.com>
2026-01-06 18:03:49 +0100

    SQLite

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Startsidan</router-link> |
     <router-link to="/project">Projekt</router-link> |
     <router-link to="/taskboard">Taskboard</router-link>
+    <pre>{{ results }}</pre>
   </nav>
 
   <router-view :model="model", :reRenderKey="reRenderKey" />
@@ -10,18 +11,48 @@
 
 <script>
 import GroupProjectHelperModel from './model.js'
+import initSqlJs from 'sql.js';
 
   export default {
     data() {
       return {
         model: new GroupProjectHelperModel(this.reRender.bind(this)),
         reRenderKey: 0,
+        results: []
       }
+    },
+    async mounted() {
+    // Run database setup when page loads
+    await this.setupDatabase();
     },
     methods: {
       reRender() {
         this.reRenderKey += 1;
       },
+
+      async setupDatabase() {
+      // Load SQL.js
+      const SQL = await initSqlJs({
+        // Optional: Path to wasm file if not bundled
+        locateFile: file => document.location.origin+`/database.wasm`
+      });
+
+      // Create in-memory database
+      const db = new SQL.Database();
+
+      // Create a table
+      db.run("CREATE TABLE users (id INTEGER, name TEXT);");
+
+      // Insert some data
+      db.run("INSERT INTO users VALUES (1, 'Alice');");
+      db.run("INSERT INTO users VALUES (2, 'Bob');");
+
+      // Query data
+      const res = db.exec("SELECT * FROM users;");
+      
+      // The results are in res[0].values
+      this.results = res[0].values;
+    }
     }
   }
 </script>
